b026257dcead204a66b23429e02778f8835ec1dc
Melk0 <36901282+Melk0@users.noreply.github.com>
2019-05-12 17:26:37 +0300

    попытка №...

--- Methods/cqrs.cpp
+++ Methods/cqrs.cpp
@@ -3,20 +3,9 @@
 #include <iostream>
 
 
-static QVector <double> masspoint1(1000);
+QVector <double> masspoint1(1000);
 
 void low (const QVector<double> &masspoint3, int firstCount, int secondCount) // фильтр нижних частот
-<<<<<<< HEAD
-{
-    int n = secondCount;
-    QVector <double> filter1(n);
-    QVector <double> filter2(n);
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter1[i] = masspoint3[i];
-    }
-=======
         {
             int n = secondCount;
             QVector <double> filter1(n);
@@ -41,130 +30,44 @@ void low (const QVector<double> &masspoint3, int firstCount, int secondCount) //
             int n = secondCount;
             QVector <double> filter1(n);
             QVector <double> filter2(n);
->>>>>>> 9cacc026dd6975f36d825dc8308a5ac732f449f3
 
-    for (int i = firstCount+12; i < secondCount; i++)
-    {
-        filter2[i] =(2 * filter2[i - 1] - filter2[i - 2] + (filter1[i] - 2 * filter1[i - 6] + filter1[i - 12]) / 32);
-    }
-    masspoint1 = filter2;
+                
+                 for (int i = firstCount; i < secondCount; i++)
+                {
+                    filter1[i] = masspoint3[i];
+                }
 
-}
 
-void high(const QVector<double> &masspoint3, int firstCount, int secondCount)// фильтр верхних частот
-{
+                for (int i = firstCount+32; i < secondCount; i++)
+                {
+                    filter2[i]= (filter2[i - 1] - filter1[i] / 32 + filter1[i - 16] - filter1[i - 17] + filter1[i - 32] / 32);
+                }
+                
 
-    int n = secondCount;
-    QVector <double> filter1(n);
-    QVector <double> filter2(n);
+                masspoint1 = filter2;
+            }
 
-<<<<<<< HEAD
-=======
         void differentiation(const QVector<double> &masspoint3, int firstCount, int secondCount)//функция дифференцирования
         {
             int n = secondCount;
             QVector <double> filter1(n);
             QVector <double> filter2(n);
->>>>>>> 9cacc026dd6975f36d825dc8308a5ac732f449f3
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter1[i] = masspoint3[i];
-    }
 
 
-    for (int i = firstCount+32; i < secondCount; i++)
-    {
-        filter2[i]= (filter2[i - 1] - filter1[i] / 32 + filter1[i - 16] - filter1[i - 17] + filter1[i - 32] / 32);
-    }
+            for (int i = firstCount; i < secondCount; i++)
+            {
+                filter1[i] = masspoint3[i];
+            }
 
 
-    masspoint1 = filter2;
-}
+            for (int i = firstCount+4; i < secondCount; i++)
+            {
+                filter2[i] = (2*filter1[i] + filter1[i-1] - filter1[i-3] - 2* filter1[i-4])/8;
+            }
 
-<<<<<<< HEAD
-void differentiation(const QVector<double> &masspoint3, int firstCount, int secondCount)//функция дифференцирования
-{
-    int n = secondCount;
-    QVector <double> filter1(n);
-    QVector <double> filter2(n);
+            masspoint1 = filter2;
+        }
 
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter1[i] = masspoint3[i];
-    }
-
-
-    for (int i = firstCount+4; i < secondCount; i++)
-    {
-        filter2[i] = (2*filter1[i] + filter1[i-1] - filter1[i-3] - 2* filter1[i-4])/8;
-    }
-
-    masspoint1 = filter2;
-}
-
-void square(const QVector<double> &masspoint3, int firstCount, int secondCount)//функция возведения в квадрат
-{
-    int n = secondCount;
-    QVector <double> filter1(n);
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter1[i] = masspoint3[i] * masspoint3[i];
-    }
-
-    masspoint1 = filter1;
-}
-
-QVector<double> integration(const QVector<double> &masspoint3, int firstCount, int secondCount)// интегрирование
-{
-    int N=100;
-    //   N = Convert.ToInt32(comboBox1.Text); измени сам. с формы должна "ширина окна" передаваться
-    int n = secondCount;
-    QVector <double> filter1(n);
-    QVector <double> filter2(n);
-
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter1[i] = masspoint3[i];
-    }
-
-    for (int i = firstCount; i < secondCount; i++)
-    {
-        filter2[i] = 0;
-    }
-
-
-    for (int i = firstCount+N; i < secondCount; i++)
-    {
-        double summa = 0;
-        for (int j = N; j >= firstCount;  j--)
-            summa = summa + filter1[i-(j)];
-        filter2[i] = summa / N;
-    }
-
-
-    masspoint1 = filter2;
-    return masspoint1;
-}
-
-QVector<double> topsQRS(const QVector<double> &mas1, const QVector<double> &mas)
-{
-    int n= mas.size();
-    QVector<double> res(n);
-    int max = 0;
-    for (int i=0; i < n-1; i++)
-    {
-        //std::cout << "0";
-        while (abs(mas[i+1] - mas[i]) >= 10)
-        {
-            //std::cout << "1";
-            if (mas1[max] < mas1[i])
-                max = i;
-            i++;
-=======
         void square(const QVector<double> &masspoint3, int firstCount, int secondCount)//функция возведения в квадрат
         {
             int n = secondCount;
@@ -215,19 +118,24 @@ QVector<double> topsQRS(const QVector<double> &mas1, const QVector<double> &mas)
         {
             int n= mas.size();
             QVector<double> res(n);
-            double max = 0;
-            for (int i=0; i < n-1; i++)
+            for (int i = 0; i< n; i ++)
             {
-                std::cout << "0";
-                while (abs(mas[i+1] - mas[i]) >= 10)
+                res[i] = 0;
+            }
+
+            for (int i=1; i < n; i++)
+            {
+                double max = 0;
+                while (abs(mas[i] - mas[i-1]) >= 10)
                 {
-                    std::cout << "1";
                     if (mas1[max] < mas1[i])
                         max = i;
                     i++;
                 }
+                if (mas1.indexOf(max)!=-1)
                 res[i] = max;
             }
+
             return res;
         }
 
@@ -245,25 +153,4 @@ QVector<double> topsQRS(const QVector<double> &mas1, const QVector<double> &mas)
                 std :: cout << result1[i] << std :: endl;
             emit sendQRSValues(result);
             emit finished();
->>>>>>> 9cacc026dd6975f36d825dc8308a5ac732f449f3
         }
-        res[i] = max;
-    }
-    return res;
-}
-
-void cQRS::doCalc(const QVector<double> &mas, int firstCount, int secondCount)
-{
-    QVector<double> result(secondCount);
-    QVector<double> result1(secondCount);
-    low(mas, firstCount, secondCount );
-    high(masspoint1, firstCount, secondCount );
-    differentiation(masspoint1, firstCount, secondCount );
-    square(masspoint1, firstCount, secondCount );
-    result = integration(masspoint1, firstCount, secondCount );
-    result1 = topsQRS(mas, result);
-//    for (int i=0; i < result1.size(); i++)
-//        std :: cout << result1[i] << std :: endl;
-    emit sendQRSValues(result, result1);
-    emit finished();
-}
